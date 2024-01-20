e64b72eb819912ceea00b0bd3b9abf65e2e42af7
RobotsOnDrugs <robotsondrugs@gmail.com>
2024-01-20 05:43:01 -0600

    Added tracking for hooked processes and the Ctrl-C handler will cleanly eject the module from all the processes it hooked. Killing the process will not result in cleanup. The Ctrl-C handler and the process callback will now safely use clones of variables instead of relying on environment variables for configuration, trace name tracking, and hooked process tracking.

--- injector/src/main.rs
+++ injector/src/main.rs
@@ -22,15 +22,12 @@ use dll_syringe::process::OwnedProcess;
 use serde::Deserialize;
 
 use ferrisetw::EventRecord;
-use ferrisetw::native::TraceHandle;
-use ferrisetw::native::EvntraceNativeError;
 use ferrisetw::parser::Parser;
 use ferrisetw::provider::EventFilter;
 use ferrisetw::provider::Provider;
 use ferrisetw::schema_locator::SchemaLocator;
 use ferrisetw::trace::RealTimeTraceTrait;
 use ferrisetw::trace::stop_trace_by_name;
-use ferrisetw::trace::TraceError;
 use ferrisetw::trace::TraceTrait;
 use ferrisetw::trace::UserTrace;
 
@@ -38,8 +35,10 @@ use windows::Win32::System::Diagnostics::Debug::DebugActiveProcess;
 use windows::Win32::System::Diagnostics::Debug::DebugActiveProcessStop;
 
 const MICROSOFT_WINDOWS_KERNEL_PROCESS_PROVIDER: &str = "22fb2cd6-0e7b-422b-a0c7-2fad1fd0e716";
+const PROCESS_START: u16 = 1;
+const PROCESS_STOP: u16 = 2;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[allow(dead_code)]
 struct FullConfig
 {
@@ -47,7 +46,7 @@ struct FullConfig
 	x86: InjectorConfig
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[allow(dead_code)]
 struct InjectorConfig
 {
@@ -85,11 +84,8 @@ fn main()
 	let configuration = injector_config.x86;
 
 	let hook_dll_name = &configuration.hook_dll_name;
-	// Changed the signature to set up for future work
 	let hook_dll_path = get_hook_dll_path(hook_dll_name);
 	let hook_dll_path = hook_dll_path.as_str();
-	env::set_var("NOBLOCKINPUT_DLL_PATH", hook_dll_path);
-	env::set_var("NOBLOCKINPUT_TRACE_NAME", configuration.trace_name);
 
 	let mut all_client_processes: HashSet<OwnedProcess> = HashSet::new();
 	for proc_name in &configuration.processes
@@ -100,137 +96,149 @@ fn main()
 			all_client_processes.insert(client_process);
 		}
 	}
-	env::set_var("NOBLOCKINPUT_PROCESSES", configuration.processes.join(";"));
 
-	let mut hooked_processes: Vec<u32> = vec![];
-	let (tx, rx) = mpsc::channel::<Vec<u32>>();
-	let all_client_processes = all_client_processes;
+	let mut hooked_processes: HashSet<u32> = HashSet::new();
 	for client_process in all_client_processes
 	{
 		let pid = client_process.pid().unwrap();
 		let syringe: Syringe = Syringe::for_process(client_process);
-		hooked_processes.push(u32::from(pid));
+		hooked_processes.insert(u32::from(pid));
 		let _payload: BorrowedProcessModule = syringe.inject(hook_dll_path).unwrap();
 		let pid_result = syringe.process().pid();
 		let name_result = syringe.process().base_name();
 		println!("{:?} {:?} [hooked existing]", pid_result.unwrap(), name_result.unwrap());
 	}
-	tx.send(hooked_processes).unwrap();
+
+	// Using a cloned hooked_process for use in the callback and a channel to communicate to the Ctrl-C handler from the callback
+	// Seems kind of messy, but it's necessary to move variables into those threads (probably)
+	let mut hooked_processes = hooked_processes.clone();
+	let (ctrl_c_tx, ctrl_c_rx) = mpsc::channel::<HashSet<u32>>();
+	ctrl_c_tx.send(hooked_processes.to_owned()).unwrap();
+
+	let process_callback = move |record: &EventRecord, schema_locator: &SchemaLocator|
+	{
+		match schema_locator.event_schema(record)
+		{
+			Ok(schema) =>
+				{
+					let parser = Parser::create(record, &schema);
+					let process_id: u32 = parser.try_parse("ProcessID").unwrap();
+					let image_name: String = parser.try_parse("ImageName").unwrap();
+					let file_name = Path::new(&image_name).file_name().unwrap();
+					let file_name = file_name.to_str().unwrap().to_string();
+					if !configuration.processes.contains(&file_name) { return; }
+					if record.event_id() == PROCESS_STOP
+					{
+						hooked_processes.remove(&process_id);
+						println!("{} {} [process exited]\n-----", process_id, file_name);
+						ctrl_c_tx.send(hooked_processes.clone()).unwrap();
+						return;
+					}
+					unsafe
+						{
+							std::thread::spawn(move ||
+								{
+									let attachment_result = DebugActiveProcess(process_id);
+									// Sometimes the process is crashy and dies very quickly and it's better to just ignore it and move on
+									if attachment_result.is_err() { return; }
+									sleep(Duration::from_millis(1000));
+									DebugActiveProcessStop(process_id).unwrap();
+								});
+						}
+					let owned_sc_client_result = OwnedProcess::from_pid(process_id);
+					// Same here: if the process goes bye-bye when we want to own it or inject into it, we march on
+					if owned_sc_client_result.is_err() { return; }
+					let owned_sc_client = owned_sc_client_result.unwrap();
+					let syringe: Syringe = Syringe::for_process(owned_sc_client);
+					let dll_path = get_hook_dll_path(&configuration.hook_dll_name);
+					let payload_result = syringe.inject(dll_path);
+					if payload_result.is_err()
+					{
+						println!("{:?}", payload_result.err().unwrap());
+						return;
+					}
+					println!("{} {} [hooked in callback]", process_id, file_name);
+					println!("-----");
+					hooked_processes.insert(process_id);
+					for proc in &hooked_processes
+					{
+						println!("Hooked PID: {}", proc);
+					}
+					println!("-----");
+					ctrl_c_tx.send(hooked_processes.clone()).unwrap();
+				},
+			Err(err) => println!("Error {:?}", err)
+		}
+	};
 
 	let process_provider = Provider::by_guid(MICROSOFT_WINDOWS_KERNEL_PROCESS_PROVIDER)
-		.add_filter(EventFilter::ByEventIds(vec![1]))
+		.add_filter(EventFilter::ByEventIds(vec![PROCESS_START, PROCESS_STOP])) // ProcessStart and ProcessStop events
 		.add_callback(process_callback)
 		.build();
 
-	let (user_trace, handle) = get_result(process_provider);
+	let trace_name = configuration.trace_name;
+	let _ = stop_trace_by_name(&trace_name);
+
+	let (user_trace, handle) = UserTrace::new().named(String::from(&trace_name)).enable(process_provider).start().unwrap();
 	let name = user_trace.trace_name();
 	println!("Trace name: {:?}", name);
 
-	std::thread::spawn(move ||
-	{
-		let status = UserTrace::process_from_handle(handle);
-		// This code will be executed when the trace stops. Examples:
-		// * when it is dropped
-		// * when it is manually stopped (either by user_trace.stop, or by the `logman stop -ets MyTrace` command)
-		println!("Trace ended with status {:?}", status);
-	});
+	let trace_name = trace_name.to_owned();
+	let dll_path = hook_dll_path.to_owned();
 
-	ctrlc::set_handler(move ||
-	{
-		let trace_name = env::var("NOBLOCKINPUT_TRACE_NAME").expect("TRACE_NAME wasn't previously set.");
-		let dll_path = env::var("NOBLOCKINPUT_DLL_PATH").expect("NOBLOCKINPUT_DLL_PATH wasn't previously set.");
-		let stopped_trace_result = stop_trace_by_name(trace_name.as_str());
-		let err_code = match stopped_trace_result
+	let do_cleanup = move ||
 		{
-			Ok(()) => 0,
-			Err(_) => -1
-		};
-		let hooked_processes = rx.recv().unwrap();
-		for pid in hooked_processes
-		{
-			let process = OwnedProcess::from_pid(pid).unwrap();
-			let module = process.find_module_by_path(dll_path.as_str()).unwrap().unwrap();
-			let syringe: Syringe = Syringe::for_process(process);
-			let module = syringe.eject(module.borrowed());
-			match module
+			println!("Closing...");
+			let trace_name = &trace_name;
+			let dll_path = &dll_path;
+			let stopped_trace_result = stop_trace_by_name(trace_name.as_str());
+			let err_code = match stopped_trace_result
 			{
-				Ok(_) =>
+				Ok(()) => 0,
+				Err(_) => -1
+			};
+			let mut hooked_processes: HashSet<u32> = HashSet::new();
+			loop
+			{
+				let current_hooked_processes_result = ctrl_c_rx.try_recv();
+				match current_hooked_processes_result
 				{
-					println!("Successfully ejected module from {:?} (pid {})", &syringe.process().base_name().unwrap(), pid);
-				},
-				Err(err) =>
-				{
-					println!("Failed to eject module from {:?} (pid {}): {}", &syringe.process().base_name().unwrap(), pid, err);
+					Ok(current_hooked_processes) => { hooked_processes = current_hooked_processes; }
+					Err(_) => { break; }
 				}
 			}
-		}
-		exit(err_code);
-	}).expect("Error setting Ctrl-C handler");
+			for pid in hooked_processes
+			{
+				let process = OwnedProcess::from_pid(pid).unwrap();
+				let module = process.find_module_by_path(dll_path.as_str()).unwrap().unwrap();
+				let syringe: Syringe = Syringe::for_process(process);
+				let module = syringe.eject(module.borrowed());
+				match module
+				{
+					Ok(_) =>
+						{
+							println!("Successfully ejected module from {:?} (pid {})", &syringe.process().base_name().unwrap(), pid);
+						},
+					Err(err) =>
+						{
+							eprintln!("Failed to eject module from {:?} (pid {}): {:?}", &syringe.process().base_name().unwrap(), pid, err);
+						}
+				}
+			}
+			exit(err_code);
+		};
+	ctrlc::set_handler(do_cleanup).expect("Error setting Ctrl-C handler");
+
+	std::thread::spawn(move ||
+		{
+			let status = UserTrace::process_from_handle(handle);
+			// This code will be executed when the trace stops. Examples:
+			// * when it is dropped
+			// * when it is manually stopped (either by user_trace.stop, or by the `logman stop -ets MyTrace` command)
+			println!("Trace {:?} ended with status {:?}", &name, status);
+		});
 
 	loop { sleep(Duration::from_millis(10)); }
-}
-
-fn get_result(provider: Provider) -> (UserTrace, TraceHandle)
-{
-	let trace_name = env::var("NOBLOCKINPUT_TRACE_NAME").expect("TRACE_NAME wasn't previously set.");
-	let th_result = UserTrace::new().named(String::from(&trace_name)).enable(provider).start();
-	match th_result
-	{
-		Ok((ref _user_trace, _trace_handle)) => { return th_result.unwrap(); },
-		Err(TraceError::EtwNativeError(err)) => match err
-		{
-			EvntraceNativeError::AlreadyExist =>
-				{
-					let provider = Provider::by_guid(MICROSOFT_WINDOWS_KERNEL_PROCESS_PROVIDER)
-						.add_filter(EventFilter::ByEventIds(vec![1]))
-						.add_callback(process_callback)
-						.build();
-					stop_trace_by_name(&trace_name).expect("Couldn't stop existing trace.");
-					return UserTrace::new().named(String::from(&trace_name)).enable(provider).start().unwrap();
-				},
-			_ => panic!("Couldn't start a trace: {:?}", err)
-		},
-		Err(err) => panic!("Couldn't start a trace: {:?}", err)
-	};
-}
-
-fn process_callback(record: &EventRecord, schema_locator: &SchemaLocator)
-{
-	let hook_targets = env::var("NOBLOCKINPUT_PROCESSES").expect("HOOK_TARGETS was not previously set");
-	let hook_targets: Vec<&str> = hook_targets.split(';').collect();
-	match schema_locator.event_schema(record)
-	{
-		Ok(schema) =>
-		{
-			let parser = Parser::create(record, &schema);
-			let process_id: u32 = parser.try_parse("ProcessID").unwrap();
-			let image_name: String = parser.try_parse("ImageName").unwrap();
-			let file_name = Path::new(&image_name).file_name().unwrap();
-			let file_name = file_name.to_str().unwrap().to_string();
-			if !hook_targets.contains(&file_name.as_str()) { return; }
-			unsafe
-			{
-				std::thread::spawn(move ||
-				{
-					let attachment_result = DebugActiveProcess(process_id);
-					// Sometimes the process is crashy and dies very quickly and it's better to just ignore it and move on
-					if attachment_result.is_err() { return; }
-					sleep(Duration::from_millis(1000));
-					DebugActiveProcessStop(process_id).unwrap();
-				});
-			}
-			// Same here: if the process goes bye-bye when we want to own it or inject into it, we march on
-			let owned_sc_client_result = OwnedProcess::from_pid(process_id);
-			if owned_sc_client_result.is_err() { return; }
-			let owned_sc_client = owned_sc_client_result.unwrap();
-			let syringe: Syringe = Syringe::for_process(owned_sc_client);
-			let dll_path = env::var("NOBLOCKINPUT_DLL_PATH").expect("DLL_PATH was not previously set");
-			let payload_result = syringe.inject(dll_path);
-			if payload_result.is_err() { println!("{:?}", payload_result.err().unwrap()); return; }
-			println!("{} {} [hooked]", process_id, file_name);
-		},
-		Err(err) => println!("Error {:?}", err)
-	}
 }
 
 fn get_hook_dll_path(hook_dll_name: &Option<String>) -> String
